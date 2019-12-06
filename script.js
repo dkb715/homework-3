ba7b41dfd5f09aed5adb8acf94c9898b6a34bf1d
Donavan Brown <dkb@Donavans-MacBook-Pro.local>
2019-12-05 23:11:08 -0500

    read me

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-var generate = document.querySelector("#submit");
+var passwordLength = confirm("Do you need a new password?");
+var specialCharacters = confirm("Do you need special characters?");
+var numericCharacters = confirm("Do you need numbers?");
+var lowerCase = confirm("Do you need lowercase letters?");
+var upperCase = confirm("Do you need capital letters?");
+var amountChar = prompt("How many characters do you want your password to be?");
+var amountCharInt = parseInt(amountChar);
+var password = "";
 
-generate.addEventListener("click", function(password) {
-  event.preventDefault();
-
-  console.log(password);
-  
-  var response = "Thank you for your submission " + nameInput.value + "! We will reach out to you at " + emailInput.value + ".";
-  submissionResponseEl.textContent = response;
-});
-String password = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-`~_=+[]{}/.,<>";
-Random textbox = new Random();
-character =
+for (var i =0, i < amountCharInt; i++{
+}
+var index = Math.floor(Math.random() * characters.length);
+for (var i =0, i < amountCharInt; i++{
+}
+var characters = ["a","A", 1, "!""a","A","!","1","b","B","c","C","d","D","e","E","f","F","g","G","h","H",
+    "I","i","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u",
+    "U","v","V","w","W","x","X","y","Y","z","Z"];
+for(var i = 0; i < 10; i++){
+  var index = Math.floor(Math.random() * characters.length); // 0, 1, 2, 3
+  var char = characters[index];
+  password = password + char
+}
