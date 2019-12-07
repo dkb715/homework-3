70a9713fb0ffdba9126c7948af87f7b9efbc23c8
Donavan Brown <dkb@Donavans-MacBook-Pro.local>
2019-12-07 17:27:27 -0500

    updTED GENERATER

--- script.js
+++ script.js
@@ -5,18 +5,45 @@ var lowerCase = confirm("Do you need lowercase letters?");
 var upperCase = confirm("Do you need capital letters?");
 var amountChar = prompt("How many characters do you want your password to be?");
 var amountCharInt = parseInt(amountChar);
+var lchar = ["a","b","c","d","e","f","g","h",
+               "i","j","k","l","m","n","o","p","q","r","s","t","u",
+               "v","w","x","y","z"];
+var hchar = ["A","B","C","D","E","F","G","H","J","K",
+                "L","M","N","O","P","Q","R","S","T","U",
+                "V","W","X","Y","Z"];
+var numbers = ["1","2","3","4","5","6","7","8","9"];
+var special = ["!","@","#","$"];
 var password = "";
+var isLchar = true;
+var isHchar = true;
+var isNumbers = true;
+var isSpecial = true;
 
-for (var i =0, i < amountCharInt; i++{
-}
-var index = Math.floor(Math.random() * characters.length);
-for (var i =0, i < amountCharInt; i++{
-}
-var characters = ["a","A", 1, "!""a","A","!","1","b","B","c","C","d","D","e","E","f","F","g","G","h","H",
-    "I","i","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u",
-    "U","v","V","w","W","x","X","y","Y","z","Z"];
-for(var i = 0; i < 10; i++){
-  var index = Math.floor(Math.random() * characters.length); // 0, 1, 2, 3
-  var char = characters[index];
-  password = password + char
-}
+var finalArray = [];
+
+if(isLchar){ 
+    finalArray = finalArray.concat(lchar);
+  }
+  if(isHchar){
+    finalArray = finalArray.concat(hchar);
+  }
+  if(isNumbers){
+    finalArray = finalArray.concat(numbers);
+  }
+  if(isSpecial){
+    finalArray = finalArray.concat(special);
+  }
+    if (amountChar < 7 || amountChar > 128){
+        alert("Outside password parameters")
+    } else {
+        for(var i = 0; i < amountCharInt; i++){
+            var index = Math.floor(Math.random() * finalArray.length); 
+            var char = finalArray[index];
+            password = password + char;
+          }
+    
+    alert(password)
+console.log(finalArray)
+        }
+
+
